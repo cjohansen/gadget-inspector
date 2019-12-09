aa17fb0927119125a7ff9e5a0686e4718726ab2d
Christian Johansen <christian@cjohansen.no>
2019-12-09 14:36:15 +0100

    Add some comments to help explain the somewhat involved message passing regime

--- extension/content-script.js
+++ extension/content-script.js
@@ -3,6 +3,11 @@
 const script = document.createElement("script");
 const ua = typeof chrome !== 'undefined' ? chrome : browser;
 
+/**
+ * This script is injected into every page opened when the extension is
+ * installed. It will be evaluated in the context of the page, and the code does
+ * NOT have access to the local scope in this script file.
+ */
 script.textContent = `window.cljs_data_browser = message => {
   window.postMessage({
     id: "cljs-data-browser",
@@ -14,6 +19,11 @@ script.textContent = `window.cljs_data_browser = message => {
 script.onload = () => script.parentNode.removeChild(script);
 (document.head || document.documentElement).appendChild(script);
 
+/**
+ * This listener relays the message from the above injected script. THIS piece
+ * of code DOES have access to the local scope in this file, and thus can reach
+ * `ua.runtime`.
+ */
 window.addEventListener('message', event => {
   if (event.data.id === "cljs-data-browser") {
     try {
@@ -24,6 +34,11 @@ window.addEventListener('message', event => {
   }
 });
 
+/**
+ * This listener receives messages from the extension panel (via the background
+ * page), and relays it to the gadget library, which runs a listener for
+ * messages on `window`.
+ */
 ua.runtime.onMessage.addListener(msg => {
   window.postMessage({
     id: "cljs-data-browser-action",
