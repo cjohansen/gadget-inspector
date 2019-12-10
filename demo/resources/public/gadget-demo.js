5aa5a17c597ff629c57a7489e5b783c89c2f0aea
Christian Johansen <christian@cjohansen.no>
2019-12-10 14:28:02 +0100

    Add a demo site

---
+++ demo/resources/public/gadget-demo.js
@@ -0,0 +1,12 @@
+var CLOSURE_UNCOMPILED_DEFINES = {"figwheel.repl.connect_url":"ws:\/\/localhost:8007\/figwheel-connect?fwprocess=41b5cd&fwbuild=dev"};
+var CLOSURE_NO_DEPS = true;
+if(typeof goog == "undefined") document.write('<script src="js-out/goog/base.js"></script>');
+document.write('<script src="js-out/goog/deps.js"></script>');
+document.write('<script src="js-out/cljs_deps.js"></script>');
+document.write('<script>if (typeof goog == "undefined") console.warn("ClojureScript could not load :main, did you forget to specify :asset-path?");</script>');
+document.write('<script>goog.require("figwheel.core");</script>');
+document.write('<script>goog.require("figwheel.main");</script>');
+document.write('<script>goog.require("figwheel.repl.preload");</script>');
+document.write('<script>goog.require("devtools.preload");</script>');
+document.write('<script>goog.require("process.env");</script>');
+document.write('<script>goog.require("gadget_demo.core");</script>');
